5013d774eb45e25527ed741ca54339e4b66298b2
VitalyShchukin <shchukin_v_v@mail.ru>
2021-11-09 10:21:50 +0300

    09 11 2021

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,8 @@
-
-class Car {
-    String Name; //name of car
-    int Speed = 51 + (int) (Math.random() * 49); //start speed random from (50;100);
-    int Acceleration = 1 + (int) (Math.random() * 19);// start acceleration from (0;20)
-
-    Car(String Name) {
-        this.Name = Name;
-        }
+class Car extends World{
+    int Speed() {
+        return (51 + (int) (Math.random() * 49)); //start speed random from (50;100)
+    }
+    int Acceleration() {
+        return (1 + (int) (Math.random() * 19)); //start acceleration from (0;20)
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,61 @@
 class Main {
-//    public static void main(String[] args) {
+    public static void main(String[] args) {
+        int N = 3;  //number of cars
+
+
+        Car car1 = new Car();
+            System.out.println(car1.Name() + " " + car1.Speed() + " " + car1.Acceleration());
+        }
+    }
+
+//            Car car1 = new Car();
+//        System.out.println(car1.Name() + " " + car1.Speed() + " " + car1.Acceleration());
+//        Car car2 = new Car();
+//        System.out.println(car2.Name() + " " + car2.Speed() + " " + car2.Acceleration());
 //
+////    Car[] arrayAccelerations = new int[N];//создать массив ускорений колличеством N
+////        for(//    int i = 0;//    i<N;i++)
+////
+////    {
+////        arrayAccelerations[i] = 1 + (int) (Math.random() * 19); // ускорение машины рандом (0;20)
+////    }
+//
+////
+////        System.out.println("Колличество участников заезда: " + N + " шт.");
+////        System.out.println("Параметры участников:");
+////        Car car[] = new Car[N]; //количесто автомобилей в заезде
+////        for (int i = 0; i < N; i++) {
+////            car[i] = new Car(arrayNames[i], arraySpeeds[i], arrayAccelerations[i]);
+////            System.out.println("Машина №" + (i + 1) + "\t- " + car[i].Name + " (нач.ск. " + car[i].Speed + "; нач.уск " + car[i].Acceleration + ")");
+////        }
+////        System.out.println("\n" + "Старт заезда. Набор скоростей до " + finishSpeed + "..........");
+////
 
 //
-//        System.out.println(World.);
-
-//    int[] arrayAccelerations = new int[N];//создать массив ускорений колличеством N
-//        for(
-//    int i = 0;
-//    i<N;i++)
 //
-//    {
-//        arrayAccelerations[i] = 1 + (int) (Math.random() * 19); // ускорение машины рандом (0;20)
-//    }
+//        Car arrayCars[] = new Car[N];//создать массив машин N
+//        for (int i = 0; i < N; i++) {
+//            arrayCars[i].Name = "dfef";
+//            arrayCars[i].Speed=10;
+//            arrayCars[i].Acceleration=20;
+//        }
+
+//        for (int i = 0; i < N; i++) {
+//            Car car[i]=new Car(car(i).ArrayNames[i], car(i).spd, car(i).acc);
+//            System.out.println("Имя- " + car(1).arrayNames[1] + "; скорость- " + car(1).spd() + "; ускорение- " + car(1).acc());
+//        }
+
+
+
+////    for (int i;i<N;i++) {
+////        Car car[i]=new Car();
+////        System.out.println(car[0].Name);}
 
 
 //        for (int i=0;i<3;i++) {
 //            Car car[i] = new Car[];
 //            System.out.println("скорость " + car[i].spd());
 //            System.out.println("уск " + car[i].acc());
-//    }
-}
 
 //        int finishSpeed = 100; //скорость на финише по условиям задания
 //        int N = 4; //указать количество участников заезда
